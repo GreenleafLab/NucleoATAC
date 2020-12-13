a95f48a3609bfa6fa5dc49f898cba99af69fa4e1
Alicia Schep <aschep@gmail.com>
2020-12-13 14:58:38 -0800

    some test updates for filenames

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,30 +10,30 @@ class NucleoATACTestCase(TestCase):
     def setUp(self):
         self.parser = nucleoatac_parser()
     def test_run(self):
-        cmd = "nucleoatac run --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/test_results/test --cores 2" 
+        cmd = "nucleoatac run --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/_results/example --cores 2"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
     def test_occ(self):
-        cmd = "nucleoatac occ --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/test_results/test --cores 2" 
+        cmd = "nucleoatac occ --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/_results/example --cores 2"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
     def test_vprocess(self):
-        cmd = "nucleoatac vprocess --out example/test_results/test --sizes example/example_results/example.nuc_dist.txt" 
+        cmd = "nucleoatac vprocess --out example/_results/example --sizes example/example_results/example.nuc_dist.txt"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
     def test_nuc(self):
-        cmd = "nucleoatac nuc --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/test_results/test --vmat example/example_results/example.VMat --cores 2" 
+        cmd = "nucleoatac nuc --bam example/example.bam --bed example/example.bed --fasta example/sacCer3.fa --out example/_results/example --vmat example/example_results/example.VMat --cores 2"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
     def test_merge(self):
-        cmd = "nucleoatac merge --occpeaks example/example_results/example.occpeaks.bed.gz --nucpos example/example_results/example.nucpos.bed.gz --out example/test_results/test" 
+        cmd = "nucleoatac merge --occpeaks example/example_results/example.occpeaks.bed.gz --nucpos example/example_results/example.nucpos.bed.gz --out example/_results/example"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
     def test_nfr(self):
-        cmd = "nucleoatac nfr --bed example/example.bed --occ_track example/example_results/example.occ.bedgraph.gz --calls example/example_results/example.nucmap_combined.bed.gz --out example/test_results/test --bam example/example.bam --fasta example/sacCer3.fa"
+        cmd = "nucleoatac nfr --bed example/example.bed --occ_track example/example_results/example.occ.bedgraph.gz --calls example/example_results/example.nucmap_combined.bed.gz --out example/_results/example --bam example/example.bam --fasta example/sacCer3.fa"
         args = self.parser.parse_args(cmd.split()[1:])
         nucleoatac_main(args)
- 
+
 class PyATACTestCase(TestCase):
     """
     Base TestCase class for pyatac commands
@@ -41,10 +41,10 @@ class PyATACTestCase(TestCase):
     def setUp(self):
         self.parser = pyatac_parser()
     def test_sizes(self):
-        cmd = "pyatac sizes --bam example/example.bam --out example/test_results/test_sizes" 
+        cmd = "pyatac sizes --bam example/example.bam --out example/_results/example_sizes"
         args = self.parser.parse_args(cmd.split()[1:])
         pyatac_main(args)
- 
+
 
 
 
